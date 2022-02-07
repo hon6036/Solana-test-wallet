9048e3f1968206a0e416ddaff7609ca2acb23c32
seunghwan <hon6036@gmali.com>
2022-02-07 16:10:47 +0900

    Solana wallet using web3.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,86 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <li>FromKey Balance:{{ fromkeybalance }}</li>
+    <li>ToKey Balance: {{ tokeybalance }}</li>
+    <button @click="reqAirdropFromKey">Airdrop To FromKey</button>
+    <button @click="reqAirdropToKey">Airdrop To ToKey</button>
+    <button @click="sendTransaction">SendTransaction</button>
   </div>
 </template>
 
 <script>
+import {
+  Connection,
+  clusterApiUrl,
+  Keypair,
+  Transaction,
+  LAMPORTS_PER_SOL,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  requestAirdrop
+} from '@solana/web3.js'
+const connection = new Connection(clusterApiUrl('testnet'))
+const fromkeypair = Keypair.generate()
+const tokeypair = Keypair.generate()
+const fromkeybalance = 0
+const tokeybalance = 0
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      fromkeybalance: fromkeybalance,
+      tokeybalance: tokeybalance
+    }
+  },
+  async mounted () {
+    this.fromkeybalance = await connection.getBalance(fromkeypair.publicKey)
+    this.tokeybalance = await connection.getBalance(tokeypair.publicKey)
+  },
+  methods: {
+    async reqAirdropFromKey () {
+      const fromAirdropSignature = await connection.requestAirdrop(
+        fromkeypair.publicKey,
+        1000000000
+      )
+      await connection.confirmTransaction(fromAirdropSignature)
+      this.fromkeybalance = await connection.getBalance(fromkeypair.publicKey)
+    },
+    async reqAirdropToKey () {
+      const fromAirdropSignature = await connection.requestAirdrop(
+        tokeypair.publicKey,
+        1000000000
+      )
+      await connection.confirmTransaction(fromAirdropSignature)
+      this.tokeybalance = await connection.getBalance(tokeypair.publicKey)
+      console.log(await connection.getBalance(tokeypair.publicKey))
+    },
+    async sendTransaction () {
+      const transaction = new Transaction()
+      transaction.add(
+        SystemProgram.transfer({
+          fromPubkey: fromkeypair.publicKey,
+          toPubkey: tokeypair.publicKey,
+          lamports: 500000000
+        })
+      )
+      const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [fromkeypair]
+      )
+      console.log(signature)
+      this.fromkeybalance = await connection.getBalance(fromkeypair.publicKey)
+      this.tokeybalance = await connection.getBalance(tokeypair.publicKey)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
